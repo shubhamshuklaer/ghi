4938b6f2804c74ab7e8140be24e495e4e40499d7
LFDM <1986gh@gmail.com>
2014-02-26 00:40:25 +0100

    Start Pull command with show subcommand

--- lib/ghi/commands.rb
+++ lib/ghi/commands.rb
@@ -19,5 +19,6 @@ module GHI
 		autoload :Status,    'ghi/commands/status'
     autoload :Unassign,  'ghi/commands/unassign'
     autoload :Version,   'ghi/commands/version'
+    autoload :Pull,      'ghi/commands/pull'
   end
 end
