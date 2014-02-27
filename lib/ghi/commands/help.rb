2e76b7a9af14c9193a1f3f46b211014124833b64
LFDM <1986gh@gmail.com>
2014-02-27 03:27:47 +0100

    Improve help messages

--- lib/ghi/commands/help.rb
+++ lib/ghi/commands/help.rb
@@ -38,6 +38,7 @@ The most commonly used ghi commands are:
    status      Determine whether or not issues are enabled for this repo
    enable      Enable issues for the current repo
    disable     Disable issues for the current repo
+   pull        Manage your pull requests
 
 See 'ghi help <command>' for more information on a specific command.
 Type 'ghi aliases' to see a list of aliases you can use to save some typing.
