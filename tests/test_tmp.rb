bc1005779f5097894cf3fa3472fa7808518178e4
Shubham Shukla <shubhamshuklaerssss@gmail.com>
2016-05-06 11:36:48 +0530

    Test token generation

--- tests/test_tmp.rb
+++ tests/test_tmp.rb
@@ -1,9 +1,15 @@
 require "test/unit"
+require "typhoeus"
 
 class TestNAME < Test::Unit::TestCase
 
-  def test_sample
-    assert_equal(4, 2+2)
+  def test_generate_token
+     ghi_exec=File.expand_path('../ghi', File.dirname(__FILE__))
+     `#{ghi_exec} config --auth --quiet`
+     token=`git config --global ghi.token`
+     assert_not_equal("",token,"Token not present in ~/.gitconfig")
+     response=Typhoeus.get("https://api.github.com/users/#{ENV['GITHUB_USER']}",headers:{'Authorization'=>"token #{token}"})
+     assert_equal('public_repo, repo',response.headers["X-OAuth-Scopes"])
   end
 
 end
