5edf8e1f9d515120480d6fc2f8dd23caf54ab285
Shubham Shukla <shubhamshuklaerssss@gmail.com>
2016-05-28 17:28:53 +0530

    Fix regex. [abc] matches any character out of a,b,c.

--- test/show_test.rb
+++ test/show_test.rb
@@ -19,9 +19,14 @@ class Test_show < Test::Unit::TestCase
         assert_match(/\A#1: #{issue[:title]}\n/,show_output,"Title not proper")
         assert_match(/^@#{ENV["GITHUB_USER"]} opened this issue/,show_output,"Opening user not proper")
         assert_match(/^@#{ENV["GITHUB_USER"]} is assigned/,show_output,"Assigned user not proper")
-        issue[:labels].each do |label|
-            assert_match(/[#{label}]/,show_output,"#{label} label not present")
+
+        labels_str=""
+        issue[:labels].sort.each do |tmp|
+            labels_str+="\\[#{tmp}\\] "
         end
+        labels_str.strip!
+
+        assert_match(/#{labels_str}/,show_output,"labels not present")
         assert_match(/Milestone #1: #{milestone[:title]}/,show_output,"Milestone not proper")
         assert_match(/@#{ENV["GITHUB_USER"]} commented/,show_output,"Comment creator not proper")
         assert_match(/#{comment}/,show_output,"Comment not proper")
